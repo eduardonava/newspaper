7ee68720b55ae61a51e951c19d7efeeb621bd52f
eduardo <eduardo92mn@gmail.com>
2017-07-11 10:21:47 -0500

    Clase 2 modificaciones en el settings

--- newspaper/newspaper/settings.py
+++ newspaper/newspaper/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newspaper.news'
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'statics')]
